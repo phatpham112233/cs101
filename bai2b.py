cd310a12ad7f54777aeba5df17a7bd9ad8b1aebe
phatpham <phatpham1302@gmai.com.com>
2024-06-03 22:28:51 +0700

    Module1/bai2/check not letter/sigmoid/relu/elu

--- bai2b.py
+++ bai2b.py
@@ -1,30 +1,30 @@
 import numpy as np
 
 def sigmoid_calc(x):
-    sigmoid = 1/(1+np.exp(-x))
+    sigmoid = 1 / (1 + np.exp(-x))
     return sigmoid
 
 def relu_calc(x):
-    relu = max(0, x)
+    relu = np.maximum(0, x)
     return relu
 
-def elu_calc(x, alpha = 1.0):
-    elu = np.where(x >=0, x, alpha * (np.exp(x)-1))
+def elu_calc(x, alpha=1.0):
+    elu = np.where(x >= 0, x, alpha * (np.exp(x) - 1))
     return elu
 
 def main():
     while True:
-        choice = input("please select functions (sigmoid, relu, elu): ").strip() .lower()
+        choice = input("Please select functions (sigmoid, relu, elu): ").strip().lower()
         if choice in ['sigmoid', 'relu', 'elu']:
             break
         else:
-            print("Invalid choice.Please type 'sigmoid ,'relu', 'elu'. ")
+            print("Invalid choice. Please type 'sigmoid', 'relu', 'elu'.")
 
     while True:
         numbers = input("Please enter numbers separated by spaces to apply the function: ")
         try:
             x = np.array([float(num) for num in numbers.split()])
-            if choice == 'sigmoid' :
+            if choice == 'sigmoid':
                 result = sigmoid_calc(x)
             elif choice == 'relu':
                 result = relu_calc(x)
@@ -33,10 +33,11 @@ def main():
             print(f"The result of applying {choice} to {x} is: {result}")
             break
         except ValueError:
-            print("Invalid input. Please enter only numbers separated by spaces. ")
+            print("Invalid input. Please enter only numbers separated by spaces.")
 
 if __name__ == "__main__":
     main()
+
 
 
      
