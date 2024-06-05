6dad9b409b906e44bedb4d2c8e2b38126cf08a4b
lisan al gaib <phatpham1302@gmail.com>
2024-06-06 02:07:05 +0700

    Module1/user input/sigmoid/relu/elu/checknotzero

---
+++ bai2.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def sigmoid_calc(x):
+    sigmoid = 1 / (1 + np.exp(-x))
+    return sigmoid
+
+def relu_calc(x):
+    relu = np.maximum(0, x)
+    return relu
+
+def elu_calc(x, alpha=1.0):
+    elu = np.where(x >= 0, x, alpha * (np.exp(x) - 1))
+    return elu
+
+def main():
+    while True:
+        choice = input("Please select functions (sigmoid, relu, elu): ").strip().lower()
+        if choice in ['sigmoid', 'relu', 'elu']:
+            break
+        else:
+            print("Invalid choice. Please type 'sigmoid', 'relu', 'elu'.")
+
+    while True:
+        numbers = input("Please enter numbers separated by spaces to apply the function: ")
+        try:
+            x = np.array([float(num) for num in numbers.split()])
+            if choice == 'sigmoid':
+                result = sigmoid_calc(x)
+            elif choice == 'relu':
+                result = relu_calc(x)
+            elif choice == 'elu':
+                result = elu_calc(x)
+            print(f"The result of applying {choice} to {x} is: {result}")
+            break
+        except ValueError:
+            print("Invalid input. Please enter only numbers separated by spaces.")
+
+if __name__ == "__main__":
+    main()
